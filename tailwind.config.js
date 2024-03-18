aacc6f3d8184da6cbee099ac08a31f1c9ee39d04
Krzysztof Dabrowski <10003141@ergo.com>
2024-03-18 09:53:57 +0100

    files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'main': ['Poppins', 'sans-serif'], // replace 'Roboto' with your font name
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
